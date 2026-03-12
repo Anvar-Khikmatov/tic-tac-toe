c36863b54b6766e05fba5b7f3d12da9280905f22
Anvar Khikmatov <Anvar-Khikmatov@users.noreply.github.com>
2026-03-12 22:49:45 +0500

    ALl done, need to clean up some mess

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const gridBox = document.body.querySelector('.grid-box');
+const playerTurnDisplay = document.querySelector('.player-turn-display');
+const newGameBtn = document.querySelector('.new-game');
+const winnerPlayer = document.querySelector('.winner');
+const winnerMessage = document.querySelector('.winner-message');
+const tiles = document.querySelectorAll('.tiles');
+
+
+gridBox.addEventListener('click', (event)=> {
+    if (!event.target.classList.contains('tiles')) return;
+    const index = event.target.dataset.index;
+    gameController.playerTurn(parseInt(index), event.target);
+})
+
+
+newGameBtn.addEventListener('click', () => {
+    gameController.reset()
+} )
+
+
+
+
 function createPlayer(name, mark){
         return {
             name: name,
@@ -6,35 +28,30 @@ function createPlayer(name, mark){
 }
 
 
-
-const gameboard = (() =>   {
+const gameboard = (() => {
     return {
     
     markTiles: function(mark, index){
-        if(this.tiles[index] === null){
-         this.tiles[index] = mark; 
-        } else{
-            console.log("Alreay marked");
-            return;
-        } 
-        
+        this.tiles[index] = mark;
     },
 
-    tiles: ["o", "o", "o", null, null, null, null, null, null],
+    tiles: [null, null, null, null, null, null, null, null, null],
 
     winCondition: [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]],
 
     condition: function(mark) {
        return this.winCondition.some(element => {
            return element.every(m => mark === this.tiles[m])})
-    },
-        
+    }, 
+
     tie: function() {
          if(this.tiles.every(t => t !== null))
             return true
     }    
 }
 })()
+
+
 
 
 const gameController = (() => {
@@ -44,36 +61,72 @@ const gameController = (() => {
     let gameover = false;
 
     return {
-        playerTurn: function(clicked){
+        playerTurn: function(clickedIndex, clickedTile){
+            console.log(clickedIndex, clickedTile);
+            console.log(currentPlayer);
+            
             if(gameover) return;
-            gameboard.markTiles(currentPlayer.mark, clicked)          
+
+            if(gameboard.tiles[clickedIndex] != null){
+                console.log("Alrady hit");                
+                return;
+            }      
+
+            clickedTile.classList.add(currentPlayer == player1 ? 'x-mark' : 'o-mark');
+            clickedTile.textContent = currentPlayer.mark;
+            playerTurnDisplay.className = currentPlayer == player1 ? 'o-turn' : 'x-turn'; 
+            playerTurnDisplay.textContent = (currentPlayer == player1 ? player2.mark : player1.mark); 
+            
+
+            gameboard.markTiles(currentPlayer.mark, clickedIndex)
+
             if(gameboard.condition(currentPlayer.mark)){
                 gameover = true;
-                return `${currentPlayer.name} is a winner`
+                console.log(`${currentPlayer.name} is a winner`);
+                const markClass = (currentPlayer == player1 ? "winner-1" : "winner-2") 
+                winnerMessage.innerHTML = `<h3>Player  <span class ="${markClass}">${currentPlayer.mark}</span>  is a winner</h3>`
+                return                
             } 
+
             if(gameboard.tie()){
                 gameover = true;
-                return `It is a tie`
+                console.log(`It is a tie`);
+                winnerMessage.innerHTML = `<h3>It's a tie!</h3>`;
+                return 
             } 
             
+    
             if(currentPlayer == player1){           
-                currentPlayer = player2
-            }else{
-                currentPlayer = player1; 
+                currentPlayer = player2;
+            }else {
+                currentPlayer = player1;
             }
+       },
+
+       reset: function(){
+
+        let i = 0;
+        while (i < gameboard.tiles.length){
+            gameboard.tiles[i] = null;
+            i++
+        }
+    
+        tiles.forEach(tile => {
+            tile.textContent = "";
+            tile.classList.remove("x-mark", "o-mark");
+            playerTurnDisplay.className = "default-turn"
+        })
+
+        currentPlayer = player1;    
+        gameover = false;
+        playerTurnDisplay.textContent = player1.mark;
+        winnerMessage.textContent = "";
        }
  }
 })()
 
 
+gameController.reset()
 
 
-// gameboard.markTiles(playerMark, index)
-// gameboard.markTiles(playerMark, index)
-console.log(gameController.playerTurn(0));
-// console.log(gameboard.tie());
-// gameController.playerTurn(0)
-// console.log(gameboard.tiles)
-// console.log(gameboard.condition("x"));
-// console.log(gameboard.markTiles("o", 0));
 
